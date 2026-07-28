6a187240d0a332a93bb65af2758b975447046668
Jycccc111 <15962199198@163.com>
2026-07-28 10:38:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from board import GoGame
-from flask import Flask,render_template,jsonify,request
-
-app=Flask(__name__)
-
-
-game=GoGame()
+from flask import Flask,render_template,jsonify,request,session
+import uuid
 
 
+app = Flask(__name__)
+
+app.secret_key = "your_secret_key"
+
+
+games = {}
+def get_game():
+
+    if "id" not in session:
+        session["id"] = str(uuid.uuid4())
+
+        games[session["id"]] = GoGame()
+
+
+    return games[session["id"]]
 
 @app.route("/")
 def index():
@@ -19,17 +30,26 @@ def index():
 
 @app.route("/move",methods=["POST"])
 def move():
-    print("收到move请求")
-    data = request.json
-    print(data)
-    x = data["x"]+1
-    y = data["y"]+1
-    success = game.move(x,y)
-    return jsonify({
-        "success":success,
-        "board":game.board.tolist()
-    })
 
+    game = get_game()
+
+
+    data=request.json
+
+    x=data["x"]+1
+    y=data["y"]+1
+
+
+    success=game.move(x,y)
+
+
+    return jsonify({
+
+        "success":success,
+
+        "board":game.board.tolist()
+
+    })
 
 if __name__ == "__main__":
     app.run(
