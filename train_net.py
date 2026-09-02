ff0b8311df47eb86056781b63d723c0f3d74df59
Jycccc111 <15962199198@163.com>
2026-09-02 19:11:38 -0400

    1

--- train_net.py
+++ train_net.py
@@ -229,7 +229,7 @@ class Policy_network(nn.Module):
 
                 loader = DataLoader(
                     dataset,
-                    batch_size=64,
+                    batch_size=512,
                     shuffle=True
                 )
 
